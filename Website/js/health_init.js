e535f02c78c7484eeea225198272e78932837644
SimonSchirber <schirber.simon@gmail.com>
2022-11-10 12:37:57 -0800

    Website functionality

--- Website/js/health_init.js
+++ Website/js/health_init.js
@@ -1,23 +1,25 @@
 var curls20lb = {'complete': 20, 'goal': 50}
-var curls20lb_trend =  [5, 10, 15, 20, 5, 6, 7]
+var curls20lb_trend =  [5, 20, 30, 42, 15, 69, 74]
 localStorage.setItem('curls20lb', JSON.stringify(curls20lb))
 localStorage.setItem('curls20lbtrend', JSON.stringify(curls20lb_trend))
 
 var curls30lb = {'complete': 30, 'goal': 40}
-var curls30lb_trend =  [5, 10, 15, 20, 5, 6, 7]
+var curls30lb_trend =  [5, 10, 15, 20, 59, 90, 73]
 localStorage.setItem('curls30lb', JSON.stringify(curls30lb))
 localStorage.setItem('curls30lbtrend', JSON.stringify(curls30lb_trend))
 
 var lunges = {'complete': 17, 'goal': 20}
-var lunges_trend =  [5, 10, 15, 20, 5, 6, 7]
+var lunges_trend =  [40, 0, 80, 100, 110, 80, 100]
 localStorage.setItem('lunges', JSON.stringify(lunges))
 localStorage.setItem('lungestrend', JSON.stringify(lunges_trend))
 
 var timeworkout = {'complete': 20, 'goal': 45}
-var timeworkout_trend =  [5, 10, 15, 20, 5, 6, 7]
+var timeworkout_trend =  [45, 10, 75, 90, 25, 68, 100]
 localStorage.setItem('timeworkout', JSON.stringify(timeworkout))
 localStorage.setItem('timeworkouttrend', JSON.stringify(timeworkout_trend))
 
+
+change_screen(1,  true)
 move_progressbar(1)
 move_progressbar(2)
 move_progressbar(3)
