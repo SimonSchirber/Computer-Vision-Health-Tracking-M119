404bb82d02addf1958267edbf2a433557f45219d
SimonSchirber <schirber.simon@gmail.com>
2022-11-10 00:26:03 -0800

    website added

---
+++ Website/js/health_init.js
@@ -0,0 +1,24 @@
+var curls20lb = {'complete': 20, 'goal': 50}
+var curls20lb_trend =  [5, 10, 15, 20, 5, 6, 7]
+localStorage.setItem('curls20lb', JSON.stringify(curls20lb))
+localStorage.setItem('curls20lbtrend', JSON.stringify(curls20lb_trend))
+
+var curls30lb = {'complete': 30, 'goal': 40}
+var curls30lb_trend =  [5, 10, 15, 20, 5, 6, 7]
+localStorage.setItem('curls30lb', JSON.stringify(curls30lb))
+localStorage.setItem('curls30lbtrend', JSON.stringify(curls30lb_trend))
+
+var lunges = {'complete': 17, 'goal': 20}
+var lunges_trend =  [5, 10, 15, 20, 5, 6, 7]
+localStorage.setItem('lunges', JSON.stringify(lunges))
+localStorage.setItem('lungestrend', JSON.stringify(lunges_trend))
+
+var timeworkout = {'complete': 20, 'goal': 45}
+var timeworkout_trend =  [5, 10, 15, 20, 5, 6, 7]
+localStorage.setItem('timeworkout', JSON.stringify(timeworkout))
+localStorage.setItem('timeworkouttrend', JSON.stringify(timeworkout_trend))
+
+move_progressbar(1)
+move_progressbar(2)
+move_progressbar(3)
+move_progressbar(4)
